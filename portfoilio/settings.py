b377a983876ce9df39cd08c6aa95c2ea9855b56d
Shyam Sundhar <clashwithchiefrpjyt@gmail.com>
2020-06-28 22:57:47 +0530

    Hey

--- portfoilio/settings.py
+++ portfoilio/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'portfoilio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pbdb',
+        'USER':'postgres',
+        'PASSWORD':'shyam1411',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
