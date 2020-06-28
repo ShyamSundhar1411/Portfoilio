434dec64cf72576685921ed196c6140f03772a09
Shyam Sundhar <clashwithchiefrpjyt@gmail.com>
2020-06-28 18:22:48 +0530

    Ready To Go

--- personal/views.py
+++ personal/views.py
@@ -5,3 +5,5 @@ from .models import Project
 def home(request):
     a = Project.objects.all()
     return render(request,'personal/home.html',{'data':a})
+def about(request):
+    return render(request,'personal/about.html')
--- portfoilio/urls.py
+++ portfoilio/urls.py
@@ -7,5 +7,6 @@ from personal import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name = 'home'),
+    path('about/',views.about,name = 'about'),
     path('blog/',include('blog.urls')),
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
