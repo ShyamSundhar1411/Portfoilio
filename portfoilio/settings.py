a959966bc435805807bbc4d34c336ef3dbacf667
Shyam Sundhar <clashwithchiefrpjyt@gmail.com>
2020-06-29 11:48:32 +0000

    Debug

--- portfoilio/settings.py
+++ portfoilio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uvhg+tuy-q_kc58nwuns^q%ft45mspkh28#9xozy!kx1p=p6r&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['shyamsundhar.pythonanywhere.com']
 
